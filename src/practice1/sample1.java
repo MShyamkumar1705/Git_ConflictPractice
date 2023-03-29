50a1e7ecd354faf3b38e67f2e0199c1b3b87df23
Shyam <samdhruv05@gmail.com>
2023-03-29 14:00:53 +0530

    commit 2

--- src/practice1/sample1.java
+++ src/practice1/sample1.java
@@ -3,7 +3,7 @@ package practice1;
 public class sample1 {
 
 	public static void main(String[] args) {
-System.out.println("My name is sheela");
-	}
+		System.out.println("My name is zahid");
+		}
 
 }
